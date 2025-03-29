2d69890acac9dfea1fcae613f4a9bbd98e13ef37
ArtemMartin <arcenivashenko@yandex.ru>
2025-03-29 16:22:57 +0300

    add clean directory

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,11 @@ package org.example;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
 
 public class Main {
     private static File file;
+
 
     public static void main(String[] args) {
         try {
@@ -15,14 +17,25 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+        cleanDirectory("D:\\YO_NA\\BP_KML\\BP\\GADn");
         file = new File("D:\\YO_NA\\BPExcel\\ADn.csv");
         makeOPKML("GADn");
 
+        cleanDirectory("D:\\YO_NA\\BP_KML\\BP\\ReADn");
         file = new File("D:\\YO_NA\\BPExcel\\READn.csv");
         makeOPKML("ReADn");
 
+        cleanDirectory("D:\\YO_NA\\BP_KML\\BP\\Minometu");
         file = new File("D:\\YO_NA\\BPExcel\\Minometu.csv");
         makeOPKML("Minometu");
+    }
+
+    private static void cleanDirectory(String path) {
+        try {
+            FileUtils.cleanDirectory(new File(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void makeOPKML(String papka) {
